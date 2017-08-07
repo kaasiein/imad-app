1dc06b1895ed3cf141508b8c23902106bd87473e
kaasiein <kaasiein@yahoo.co.in>
2017-08-07 18:36:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.get('/artical-three', function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three'));
 })
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
